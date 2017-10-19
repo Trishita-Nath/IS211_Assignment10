39eed72bc523d168aae86b4025045b84c107e9dd
Trishita-Nath <Trishita.Nath15@gmail.com>
2017-10-19 01:29:30 -0400

    Update music.sql

--- music.sql
+++ music.sql
@@ -2,8 +2,8 @@ CREATE TABLE artist (artist_id INTEGER PRIMARY KEY, artist TEXT);
 		
 CREATE TABLE album (album_id INTEGER PRIMARY KEY, album TEXT, artistID INTEGER);
 
-CREATE album_song (id INTEGER PRIMARY KEY, album_id, song_id ) ;	
+CREATE TABLE album_song (id INTEGER PRIMARY KEY, album_id, song_id);	
 
-CREATE TABLE song ( song_id INTEGER PRIMARY KEY, song TEXT, artistID INTEGER, trackNum INTEGER, trackLen INTEGER);
+CREATE TABLE song (song_id INTEGER PRIMARY KEY, song TEXT, artistID INTEGER, trackNum INTEGER, trackLen INTEGER);
 
 
